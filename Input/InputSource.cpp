89ce5c08ab6b7136115bb6d1013b93526cc65420
JJ <Magnanix@users.noreply.github.com>
2017-09-18 18:46:59 -0400

    No.

--- Input/InputSource.cpp
+++ Input/InputSource.cpp
@@ -4,9 +4,9 @@
 
 int main()	//Start of main program
 {
-	cout << "Please enter your first name (followed by 'enter'):\n";	//Character Output Stream, identifies input of USERs name
+	cout << "Please enter your first name (followed by 'enter'):\n";	//Character Output Stream, identifies input of USERs name. A 'prompt' if you will.
 	string first_name;	//String save for USERs name
-	cin >> first_name;	//What is this, I have no idea, send halp.	//Apparently the exact opposince to cout, how oblivious am I. Fetch command for what first_name is for next output I'm assuming.
+	cin >> first_name;	//What is this, I have no idea, send halp.	//Apparently the exact opposince to cout, how oblivious am I. Fetch command for first_name next output I'm assuming.
 	cout << "Your first name" << " is " << first_name << "?\n" << "Hello, " << first_name << "!\n";	//Multiple Character Output Streams on a single line instead of repetative multi line outputs, don't mind either way since learning but apparently it's good practice to never start multiple line outputs. Ok then.
 	keep_window_open();	//Keeps the damn window open because Windows Code is rubish, this shouldn't be needed but whatever, can understand why if I think broadly.
 
